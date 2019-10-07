a4f30d02648def79932c962b72f29faa485a875c
Andrew Thiessen <squaids449@gmail.com>
2019-10-07 16:18:30 -0600

    Done.

---
+++ app.js
@@ -0,0 +1,43 @@
+const body = document.body
+const screen = document.querySelector("#screen")
+const spans = document.querySelectorAll("span:not(.operator)");
+const clear = document.querySelector("#clear")
+const operators = document.querySelectorAll(".operator:not(#clear):not(#equals)")
+const equals = document.querySelector("#equals")
+let stored = ""
+
+function formatOperators(items){
+    items.forEach(item => {
+        if(item.textContent === "÷"){
+            item.value = "/"
+        }
+        if(item.textContent === "x"){
+            item.value = "*"
+        }
+        else{
+            item.value = item.textContent
+        }
+    })
+}
+formatOperators(operators)
+
+spans.forEach(span => {
+    span.addEventListener("click", event => {
+        screen.textContent += event.target.innerText
+    })
+})
+
+clear.addEventListener("click", () => {
+    screen.textContent = ""
+})
+
+operators.forEach(item => {
+    item.addEventListener("click", (event) => {
+        screen.textContent += ` ${event.target.value} `
+    })
+})
+
+equals.addEventListener("click", () => {
+    stored = screen.textContent
+    screen.textContent = eval(stored)
+})
